6c8b8c9fc28548953947c8fffe6b703fd9648645
Joe Graham <jmg3484@rit.edu>
2017-06-09 10:12:33 -0400

    small syntax fixes to shell script

--- core/roles/common/files/user-create.sh
+++ core/roles/common/files/user-create.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # user_create.sh - Shell script to create users from a csv
 
 cat /tmp/user-create.csv | grep -v "#" > /tmp/user-create.csv.1
@@ -6,8 +8,7 @@ mv /tmp/user-create.csv.1 /tmp/user-create.csv
 # Sudoers group differs between RedHat and Debian-based distros. This gets
 # around that.
 
-group_command="cat /etc/sudoers | grep -v '#' | grep '%' | awk -F \
-'[[:space:]]+' '{print $1}' | cut -d '%' -f2"
+#group_command="cat /etc/sudoers | grep -v '#' | grep '%' | awk -F '[[:space:]]+' '{print \$1}' | cut -d '%' -f2"
 
 while read line
 do
@@ -15,7 +16,8 @@ do
     password=$(echo $line | cut -d ',' -f2)
     admin=$(echo $line | cut -d ',' -f3)
     if [ "$admin" = "y" ]; then
-        group=$($group_command)
+        group=$(cat /etc/sudoers | grep -v '#' | grep '%' | awk -F '[[:space:]]+' '{print $1}' | cut -d '%' -f2)
+
 #        useradd -G $group $username
         echo "Group: $group Username: $username"
     else
