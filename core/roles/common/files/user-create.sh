af4c3b7f625ccf51c8f0cd1ee69b6db7b75030d1
Joe Graham <jmg3484@rit.edu>
2017-06-09 10:41:01 -0400

    various testing fixes

--- core/roles/common/files/user-create.sh
+++ core/roles/common/files/user-create.sh
@@ -2,28 +2,21 @@
 
 # user_create.sh - Shell script to create users from a csv
 
-cat /tmp/user-create.csv | grep -v "#" > /tmp/user-create.csv.1
-mv /tmp/user-create.csv.1 /tmp/user-create.csv
-
-# Sudoers group differs between RedHat and Debian-based distros. This gets
-# around that.
-
-#group_command="cat /etc/sudoers | grep -v '#' | grep '%' | awk -F '[[:space:]]+' '{print \$1}' | cut -d '%' -f2"
-
 while read line
 do
     username=$(echo $line | cut -d ',' -f1)
     password=$(echo $line | cut -d ',' -f2)
     admin=$(echo $line | cut -d ',' -f3)
     if [ "$admin" = "y" ]; then
-        group=$(cat /etc/sudoers | grep -v '#' | grep '%' | awk -F '[[:space:]]+' '{print $1}' | cut -d '%' -f2)
-
-#        useradd -G $group $username
-        echo "Group: $group Username: $username"
+        # The sudoers group varies between Red Hat and Debian based distros.
+        # This automatically finds out what group is specified in /etc/sudoers.
+        group=$(cat /etc/sudoers | grep -v '#' | grep '%' \
+        | awk -F '[[:space:]]+' '{print $1}' | cut -d '%' -f2)
+        useradd -G $group $username
     else
-#        useradd $username
-        echo "User: $username"
+        useradd $username
     fi
-#    yes $password passwd $username
-        echo "Password: $password"
+    # This is an old unix utility that just continuously echoes out the
+    # password into the passwd utility.
+    yes $password | passwd $username
 done < /tmp/user-create.csv
