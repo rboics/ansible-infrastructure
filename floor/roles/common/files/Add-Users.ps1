34d81393e45af8a24e61db8377deb162faf098cf
Joe Graham <jmg3484@rit.edu>
2017-06-09 16:28:30 -0400

    rework administrator promote, needs to be more specific

--- floor/roles/common/files/Add-Users.ps1
+++ floor/roles/common/files/Add-Users.ps1
@@ -1,11 +1,7 @@
-$Users = Import-Csv -Path "C:\Temp\user-create.csv"
+$Users = Import-Csv -Path "C:\user-create.csv"
 
 foreach ($Line in $Users) {
     $Username = $Line.username
     $Password = $Line.password
-    $Admin = $line.admin
     & NET USER $Username $Password /add /y
-    If ($Admin == "y") {
-        & NET LOCALGROUP "Administrators" $Username /add
-    }
 }
