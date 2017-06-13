4a77d4cd5a1d3d8f61a75b65b83197d21dc63a70
Joe Graham <jmg3484@rit.edu>
2017-06-13 15:01:56 -0400

    XP doesn't support Ansible, this batch script will create the users that we need

---
+++ floor/user-create-xp.bat
@@ -0,0 +1,26 @@
+# user-create-xp.bat - Creates the users on the old nursing workstation.
+net user BillAdmin /add
+net user BillAdmin 8kU7%Sq
+net user NRSAdmin /add
+net user NRSAdmin GxV96BW2bfTarNd
+net localgroup Administrators NRSAdmin /add
+net user mgmtadmin /add
+net user mgmtadmin admin
+net user aanderson /add
+net user aanderson 2mN8q
+net user brobertson /add
+net user brobertson n9h5G^n7BjmM
+net user ccarlson /add
+net user ccarlson bowwow
+net user ddavidson /add
+net user ddavidson X9UgU8kjetHDEFV
+net user eerickson /add
+net user eerickson y8P6rmr
+net localgroup Administrators eerickson /add
+net user ffinch /add
+net user ffinch honeybun
+net localgroup Administrators ffinch /add
+net user ggregorivich /add
+net user ggregorivich bR9&vD3
+net user iibanez /add
+net user iibanez summertime
