a7f7cfc13ac7bc299012183918a6e40e013f1d24
Joe Graham <jmg3484@rit.edu>
2017-06-13 15:59:48 -0400

    syntax fixes

--- floor/user-create-xp.bat
+++ floor/user-create-xp.bat
@@ -1,6 +1,6 @@
-# user-create-xp.bat - Creates the users on the old nursing workstation.
+REM user-create-xp.bat - Creates the users on the old nursing workstation.
 net user BillAdmin /add
-net user BillAdmin 8kU7%Sq
+net user BillAdmin 8kU7%%Sq
 net user NRSAdmin /add
 net user NRSAdmin GxV96BW2bfTarNd
 net localgroup Administrators NRSAdmin /add
@@ -9,7 +9,7 @@ net user mgmtadmin admin
 net user aanderson /add
 net user aanderson 2mN8q
 net user brobertson /add
-net user brobertson n9h5G^n7BjmM
+net user brobertson n9h5G^^n7BjmM
 net user ccarlson /add
 net user ccarlson bowwow
 net user ddavidson /add
@@ -21,6 +21,6 @@ net user ffinch /add
 net user ffinch honeybun
 net localgroup Administrators ffinch /add
 net user ggregorivich /add
-net user ggregorivich bR9&vD3
+net user ggregorivich bR9^&vD3
 net user iibanez /add
 net user iibanez summertime
